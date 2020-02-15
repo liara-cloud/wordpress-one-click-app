f11b498c326e9052262c28489d86592dae699510
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-02-15 13:26:38 +0330

    Fix newline

--- wrapper-entrypoint.sh
+++ wrapper-entrypoint.sh
@@ -3,6 +3,6 @@
 set -m
 
 echo 'Configuring php.ini ...'
-echo $PHP_INI_CONFIG > /usr/local/etc/php/conf.d/liara_php.ini
+echo "$PHP_INI_CONFIG" > /usr/local/etc/php/conf.d/liara_php.ini
 
 /usr/local/bin/docker-entrypoint.sh apache2-foreground
