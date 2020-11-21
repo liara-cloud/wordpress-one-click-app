d949353ee31be2925c0c8dc463c86d7844e2530f
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-11-21 12:08:32 +0330

    WIP

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t liararepo/wordpress-one-click-app:5-php7.2 --build-arg THE_PHP_VERSION=7.2 .
-docker build -t liararepo/wordpress-one-click-app:5-php7.3 --build-arg THE_PHP_VERSION=7.3 .
-docker build -t liararepo/wordpress-one-click-app:5-php7.4 --build-arg THE_PHP_VERSION=7.4 .
+docker build -t liararepo/wordpress-one-click-app:5-php7.2 --build-arg PHP_VERSION=7.2 .
+docker build -t liararepo/wordpress-one-click-app:5-php7.3 --build-arg PHP_VERSION=7.3 .
+docker build -t liararepo/wordpress-one-click-app:5-php7.4 --build-arg PHP_VERSION=7.4 .
